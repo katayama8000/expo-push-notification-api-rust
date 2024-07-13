a5f69ad34a9e90a010a0822503cce0c04d64498a
katayama8000 <63860688+katayama8000@users.noreply.github.com>
2024-07-13 20:07:06 +0900

    Cron (#19)
    * debug
    
    * 25
    
    * fix
    
    * debug

--- send_push_notification_to_partner.sh
+++ send_push_notification_to_partner.sh
@@ -1,5 +1,5 @@
 curl -X POST https://expo-push-notification-api-rust.vercel.app/api/handler -H "Content-Type: application/json" -d '{
   "title": "cat",
   "body": "meow",
-  "expo_push_tokens": "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]"
+  "expo_push_token": "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]"
 }'
